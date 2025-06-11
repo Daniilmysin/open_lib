a6571f56ad61f3edadfa55746130b2ab5f008aa6
apelsin <mysya.d@ya.ru>
2025-06-11 19:51:21 +0200

    fixed some shit

--- bot/Main.py
+++ bot/Main.py
@@ -8,7 +8,7 @@ from Handlers.debug import info
 from Handlers.user import add_book, add_author, other
 from models import db_act
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", filename='log')
 
 
 # Запуск бота
@@ -17,18 +17,22 @@ async def main():
         load_dotenv()
         Bot_token = str(os.getenv('bot'))
         if Bot_token is None:
-            exit(print('Bot token does not exist'))
-    except Exception:
-        exit(print(f".env file read error"))
+            exit(logging.debug('Bot token does not exist'))
 
-    bot = Bot(token=Bot_token)
+    except Exception as error_message:
+        exit(logging.error(error_message))
+
+    try:
+        bot = Bot(token=Bot_token)
+    except Exception as error_message:
+        exit(logging.error(error_message))
 
     try:
         await db_act.make_bd()
-    except Exception as error:
-        print(f"Ошибка создания базы данных: {error}")
+    except Exception as error_message:
+        logging.debug(f"create database erorr: {error_message}")
         exit()
-    # Диспетчер
+
     dp = Dispatcher()
     dp.include_routers(add_book.rt, other.rt, info.rt, add_author.rt)
     await dp.start_polling(bot)  # запускаем

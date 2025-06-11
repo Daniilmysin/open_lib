e3f121b21cc05632b45cebde37a2fe41a107fed2
apelsin <mysya.d@ya.ru>
2025-06-11 18:03:05 +0200

    someshit

--- bot/Main.py
+++ bot/Main.py
@@ -8,22 +8,21 @@ from Handlers.debug import info
 from Handlers.user import add_book, add_author, other
 from models import db_act
 
-try:
-    load_dotenv()
-    Bot_token = str(os.getenv('bot'))
-except Exception:
-    print(f".env file read error")
-    exit()
-
-bot = Bot(token=Bot_token)
-
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 
 # Запуск бота
 async def main():
-    # Объект бота
+    try:
+        load_dotenv()
+        Bot_token = str(os.getenv('bot'))
+        if Bot_token is None:
+            exit(print('Bot token does not exist'))
+    except Exception:
+        exit(print(f".env file read error"))
+
+    bot = Bot(token=Bot_token)
+
     try:
         await db_act.make_bd()
     except Exception as error:
